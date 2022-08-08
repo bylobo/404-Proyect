1fa607b4f756a399b15f529ee951accc7fed4ca9
Bylobo <carloamarin16a6@gmail.com>
2022-08-08 13:39:14 -0400

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="not-found">404 NOT FOUND</p>
+      <div className="description">
+        <h1>I have bad news for you</h1>
+        <p className="the-page">
+          The page you are looking for might be removed or is temporarily
+          unavailable
+        </p>
+        <button>BACK TO HOMEPAGE</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
